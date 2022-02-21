f7b9ac5249bab651a08cc2c81e504c29bf4b8e84
Veselin <vesselin60@gmail.com>
2022-02-21 09:23:28 +0200

    initial commit

---
+++ src/About.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+const divStyle = {
+	color: "white",
+};
+
+class About extends Component {
+	render() {
+		return (
+			<div style={divStyle}>
+				<h2>About Page</h2>
+				<main>
+					<p>This section contains information about...</p>
+				</main>
+			</div>
+		);
+	}
+}
+
+export default About;
