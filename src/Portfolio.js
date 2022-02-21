d6032d20c6c256c3a08e99046cad00edfc83eb38
Veselin <vesselin60@gmail.com>
2022-02-21 10:30:28 +0200

    Portfolio modifyed

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -10,12 +10,13 @@ const Portfolio = () => {
 				target="_blank"
 				rel="noopener noreferrer"
 			>
-				<img
+				Portfolio
+				{/* <img
 					src={require("../src/imgs/ecommerce.png")}
 					alt="me"
 					width="500px"
 					height="300px"
-				/>
+				/> */}
 			</a>
 			<Link to="/">Home</Link>
 		</div>
